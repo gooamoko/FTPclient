07a25cf3b282e9405cbf30b4bfe397db41499edf
voronin.leonid@otr.ru <voronin.leonid@otr.ru>
2020-03-06 00:54:20 +0700

    Пробуем работать данными, которые отправляет пользователь

--- app/src/main/java/ru/gooamoko/ftpclient/MainActivity.java
+++ app/src/main/java/ru/gooamoko/ftpclient/MainActivity.java
@@ -2,35 +2,72 @@ package ru.gooamoko.ftpclient;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class MainActivity extends AppCompatActivity {
     private static final String CHANNEL_ID = "FTPCLNT";
 
-    private EditText userEdit;
+    private EditText folderEdit;
+    private TextView errorView;
     private int notificationId;
 
     @Override
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userEdit = findViewById(R.id.userEdit);
+        folderEdit = findViewById(R.id.userEdit);
+        errorView = findViewById(R.id.errorView);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            ClipData clipData = intent.getClipData();
+            if (clipData == null) {
+                errorView.setText("No URI received");
+            } else {
+                int itemCount = clipData.getItemCount();
+
+                if (itemCount > 0) {
+                    ClipData.Item item = clipData.getItemAt(0);
+                    File file = getFile(item.getUri());
+                    if (file != null && file.canRead()) {
+                        String message = String.format("%s (%s)", file.getAbsolutePath(), getMimeType(item.getUri()));
+                        errorView.setText(message);
+                    } else {
+                        errorView.setText("Can't read " + file.getAbsolutePath());
+                    }
+                } else {
+                    errorView.setText("No items found");
+                }
+            }
+        } else {
+            errorView.setText("Intent is null!");
+        }
 
         Button exitButton = findViewById(R.id.exitButton);
         exitButton.setOnClickListener(new View.OnClickListener() {
@@ -44,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                testPreferences();
+                uploadFile();
             }
         });
 
@@ -73,7 +110,30 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
-    private void testPreferences() {
+    private File getFile(Uri uri) {
+        try {
+            if (uri != null) {
+                URI fileUri = new URI(uri.toString());
+                return new File(fileUri);
+            }
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getMimeType(Uri uri) {
+        String type = null;
+        if (uri != null) {
+            String extension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
+            if (extension != null) {
+                type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+            }
+        }
+        return type;
+    }
+
+    private void uploadFile() {
         SharedPreferences preferences = getSharedPreferences(FtpClient.PREFERENCES_NAME, MODE_PRIVATE);
         String host = preferences.getString(FtpClient.HOST, "");
         String port = preferences.getString(FtpClient.PORT, "21");
